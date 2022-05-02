1056cb2f2b3fdd57a293ede897433a83d974ed29
raul macedo fuzita <raul_fuzita@hotmail.com>
2022-05-02 22:26:44 +0100

    init

---
+++ spv/src/main/java/com/spv/HelloWorld.java
@@ -0,0 +1,33 @@
+package com.spv;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
+public class HelloWorld extends HttpServlet {
+
+	@Override
+	public void service(HttpServletRequest req, HttpServletResponse res) 
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// super.service(req, res);
+		
+		PrintWriter out = res.getWriter();
+		
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>First Servlet</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>Hello World!</h1>");
+		out.println("<p>Testing Servelet</p>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
---
+++ spv/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<!DOCTYPE html">
+<html lang="en">
+<header>
+	<title>Test</title>
+</header>
+<body>
+<h2>Hello World!</h2>
+</body>
+</html>
